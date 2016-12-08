a82f08899d373bbc381f7be8811516a3755926ad
Michel Barbou Salvador <mibarbou@gmail.com>
2016-12-08 15:33:36 +0100

    Initial AndroidStudio project

---
+++ JunkFood/app/src/main/java/com/mibarbou/junkfood/TableListActivity.java
@@ -0,0 +1,13 @@
+package com.mibarbou.junkfood;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TableListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_table_list);
+    }
+}
